4500cefdb153cb0099b89f5b2cce2ace4dbc3791
Patrick Hill <patrickrhill@gmail.com>
2020-06-15 16:59:54 -0400

    adds keyboard shortcut, removes menu cause it's not needed

--- saver.py
+++ saver.py
@@ -9,21 +9,22 @@ bl_info = {
 
 
 import bpy
-
 from os.path import dirname
 import time
 
 
-def main(context):
+def main(context,self):
     format = bpy.data.scenes['Scene'].render.image_settings.file_format
     if format == 'OPEN_EXR_MULTILAYER': extension = '.exr'
     if format == 'JPEG': extension = '.jpg'
     if format == 'PNG': extension = '.png'
     
+    
     try:
         bpy.data.images['Render Result'].save_render(dirname(bpy.data.filepath) + '/exports/' + time.strftime("%y%m%d") + "-" + time.strftime("%H%M%S") + extension, scene=None)
+        self.report({'INFO'}, "Image Saved")
     except:
-        print('No renders yet. You must render an image before you can save it.')
+        self.report({'INFO'}, "You must render an image before saving")
     
 
 class Saver(bpy.types.Operator):
@@ -31,34 +32,30 @@ class Saver(bpy.types.Operator):
     bl_label="save rendered file"
     
     def execute(self, context):
-        main(context)
+        main(context,self)
         return {'FINISHED'}
 
 
 
+addon_keymaps = []
 
-class SaverMenu(bpy.types.Menu):
-    bl_label = "Layout Demo"
-    bl_idname = "viewImage.saverMenu"
-
-    def draw(self, context):
-        layout = self.layout
-
-        layout.label(text="Save in background to //exports:")
-        row = layout.row()
-        row.scale_y = 3.0
-        row.operator("myops.saver")
-    
-        
 
 def register():
     bpy.utils.register_class(Saver)
-    bpy.utils.register_class(SaverMenu)
-#    bpy.ops.wm.call_menu(name=SaverMenu.bl_idname)
+
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name="Image", space_type="IMAGE_EDITOR")
+        kmi = km.keymap_items.new("myops.saver", type="S", value="PRESS", shift=True, ctrl=True, oskey=True)
+        addon_keymaps.append((km, kmi))
 
 def unregister():
+    for km,kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
     bpy.utils.unregister_class(Saver)
-    bpy.utils.unregister_class(SaverMenu)
 
 
 if __name__ == "__main__":
